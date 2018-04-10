f85ee8a8de01d83cb9ec5543ffffa85bb3bc0676
rjiang98 <richard.jiang98@gmail.com>
2018-04-09 20:15:11 -0700

    First Attempt at login system

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const app = require('express')(),
-      server = require('http').Server(app);
+    server = require('http').Server(app);
 const knex = require('knex')(require('./knexfile'))
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (request, response) => {
-      response.status(200).json({ Welcome: 'Colourhood presents the InstaMessage project' });
+    response.status(200).json({ Welcome: 'Colourhood presents the InstaMessage project' });
+});
+app.post('/createUser', (request, response) => {
+    knex
+        .createUser({
+            username: request.body.username,
+	    password: request.body.password
+        })
+	.then(() => response.sendStatus(200);)
+});
+app.post('/login', (request, response) => {
+    knex
+    	.authenticate({
+	    username: request.body.username,
+	    password: request.body.password
+	})
+	.then(({ success }) => {
+	    if (success) {
+		response.sendStatus(200);
+	    } else {
+		response.sendStatus(401);
+	    }
+	})
 });
 
 server.listen(PORT, () => {
-      console.log(`Server running on port: ${PORT}`);
+    console.log(`Server running on port: ${PORT}`);
 });
