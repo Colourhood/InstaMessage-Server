cec3cc85b3559b07d312546cfad81243f8a74f60
Andrei <andrei.villasana@smashingideas.com>
2018-04-11 19:03:47 -0700

    changed structure

--- store.js
+++ store.js
@@ -1,22 +1,19 @@
 const knex = require('knex')(require('./knexfile'));
 
-module.exports = {
-    createUser ({ username, password }) {
-        console.log('Add user ${username} with password ${password}');
-	return knex('user').insert({
-	    username,
-	    password
-	});
-    },
-    authenticate({ username, password }) {
-	console.log('Authenticating user ${username}');
+function createUser({username, password}) {
+	return knex('user').insert({ username, password });
+};
+
+function authenticate({username, password}) {
+	console.log(`Authenticating user ${username}`);
 	return knex('user').where({ username })
-	    .then(([user]) => {
-		if (!user) {
-		    return { success: false };
-		} else {
-		    return { success: password === user.password };
-		}
-	    });
-    }
-}
+		.then(([user]) => {
+			if (!user) {
+				return { success: false };
+			} else {
+				return { success: password === user.password };
+			}
+		});
+};
+
+module.exports = { createUser, authenticate }
