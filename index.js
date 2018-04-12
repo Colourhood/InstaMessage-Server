3a39fff053260f1f081eb2b01074c927e5cf89d1
rjiang98 <richard.jiang98@gmail.com>
2018-04-11 21:22:26 -0700

    Fix file structure

--- index.js
+++ index.js
@@ -12,30 +12,6 @@ app.use('/user', user);
 app.get('/', (request, response) => {
     response.status(200).json({ Welcome: 'Colourhood presents the InstaMessage project' });
 });
-<<<<<<< HEAD
-=======
-app.post('/createUser', (request, response) => {
-	console.log(request.body.username)
-	console.log(request.body.password)
-
-    store.createUser({ username: request.body.username, password: request.body.password })
-		.then(() => response.sendStatus(200));
-});
-app.post('/login', (request, response) => {
-    store
-    	.authenticate({
-	    username: request.body.username,
-	    password: request.body.password
-	})
-	.then(({ success }) => {
-	    if (success) {
-		response.sendStatus(200);
-	    } else {
-		response.sendStatus(401);
-	    }
-	})
-});
->>>>>>> 80290fce7190d698d4dc19dd55d0bca03dffe013
 
 server.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
