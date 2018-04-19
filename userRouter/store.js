1ded1e1702169862374ce97e0575680a41ef28b7
rjiang98 <richard.jiang98@gmail.com>
2018-04-18 19:31:48 -0700

    Try 3

--- userRouter/store.js
+++ userRouter/store.js
@@ -1,5 +1,5 @@
 const crypto = require('crypto');
-const knex = require('knex')(require('knexfile'));
+const knex = require('knex')(require('../knexfile'));
 
 function createUser({username, password}) {
 	console.log(`A new user was created ${username}`);
