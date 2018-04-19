06be22e177cebdf7cf03bc5fa465948ae763aead
rjiang98 <richard.jiang98@gmail.com>
2018-04-18 19:20:04 -0700

    Migration try

--- migrations/20180416195653_encrypt_user_password.js
+++ migrations/20180416195653_encrypt_user_password.js
@@ -1,32 +1,32 @@
-const { saltHashPassword } = require('../userRouter/store');
+const { saltHashPassword } = require('/userRouter/store')
 
 exports.up = function(knex, Promise) {
 	return knex.schema.table('user', t => {
-		t.string('salt').notNullable();
-		t.string('encrypted_password').notNullable();
+		t.string('salt').defaultTo('default').notNullable()
+		t.string('encrypted_password').defaultTo('default').notNullable()
 	})
 	.then(() => knex('user'))
 	.then(users => Promise.all(users.map(convertPassword)))
 	.then(() => {
 		return knex.schema.table('user', t => {
 			t.dropColumn('password')
-		});
-	});
+		})
+	})
 
 	function convertPassword (user) {
-		const { salt, hash } = saltHashPassword(user.password);
+		const { salt, hash } = saltHashPassword(user.password)
 		return knex('user').where({ id: user.id })
 			.update({
 				salt,
 				encrypted_password: hash
-			});
+			})
 	}
-};
+}
 
 exports.down = function(knex, Promise) {
 	return knex.schema.table('user', t => {
-		t.dropColumn('salt');
-		t.dropColumn('encrypted_password');
-		t.string('password').notNullable();
-	});
-};
+		t.dropColumn('salt')
+		t.dropColumn('encrypted_password')
+		t.string('password').notNullable()
+	})
+}
