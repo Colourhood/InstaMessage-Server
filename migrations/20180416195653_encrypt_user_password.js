4b411100848531d0886ff000f25f470a27604df3
rjiang98 <richard.jiang98@gmail.com>
2018-04-18 20:11:24 -0700

    Just created columns

--- migrations/20180416195653_encrypt_user_password.js
+++ migrations/20180416195653_encrypt_user_password.js
@@ -5,26 +5,11 @@ exports.up = async function up (knex) {
 		t.string('salt')
 		t.string('encrypted_password')
 	})
-	const users = await knex('user')	
-	await Promise.all(users.map(convertPassword))
-	await knex.schema.table('user', t => {
-		t.dropColumn('password')
-	})
-
-	function convertPassword (user) {
-		const { salt, hash } = saltHashPassword(user.password)
-		return knex('user').where({ id: user.id })
-			.update({
-				salt,
-				encrypted_password: hash
-			})
-	}
 }
 
 exports.down = function(knex, Promise) {
 	return knex.schema.table('user', t => {
 		t.dropColumn('salt')
 		t.dropColumn('encrypted_password')
-		t.string('password').notNullable()
 	})
 }
