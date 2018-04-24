46b2595a6adaf6c0f4757028b86777bcb1ff00f9
rjiang98 <richard.jiang98@gmail.com>
2018-04-23 19:43:10 -0700

    New Migration

---
+++ migrations/20180423191616_remove_password_make_encryption_nonnullable.js
@@ -0,0 +1,30 @@
+const { saltHashPassword } = require('../routes/user/store')
+
+exports.up = async function up (knex) {
+  const users = await knex('user');
+  await Promise.all(users.map(convertPassword));
+  await knex.schema.table('user', t=> {
+    t.dropColumn('password');
+  });
+  await knex.schema.alterTable('user', t => {
+    t.string('salt').nonNullable().alter();
+    t.string('encrypted_password').nonNullable().alter();
+  });
+
+  function convertPassword (user){
+    const { salt, hash } = saltHashPassword(user.password);
+    return knex(user).where({ username: user.username })
+      .update({
+        salt,
+        encrypted_password: hash
+      });
+  }
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.table('user', t => {
+    t.dropColumn('salt')
+    t.dropColumn('encrypted_password')
+    t.string('password').notNullable()
+  });
+};
