2df9f553c0a92138ae13189db4c3abf5e4d16aaa
rjiang98 <richard.jiang98@gmail.com>
2018-04-23 20:16:47 -0700

    Removed trying to convert passwords

--- migrations/20180423191616_remove_password_make_encryption_nonnullable.js
+++ migrations/20180423191616_remove_password_make_encryption_nonnullable.js
@@ -1,24 +1,19 @@
 const { saltHashPassword } = require('../routes/user/store')
 
 exports.up = async function up (knex) {
-  const users = await knex('user');
-  await Promise.all(users.map(convertPassword));
   await knex.schema.table('user', t=> {
     t.dropColumn('password');
   });
-
-  function convertPassword (user) {
-    const { salt, hash } = saltHashPassword(user.password);
-    return knex(user).where({ id: user.id })
-      .update({
-        salt,
-        encrypted_password: hash
-      });
-  }
+  await knex.schema.alterTable('user', t => {
+    t.string('salt').nonNullable().alter();
+    t.string('encrypted_password').nonNullable().alter();
+  });
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.table('user', t => {
+  return knex.schema.alterTable('user', t => {
+    t.string('salt').nullable().alter()
+    t.string('encrypted_password').nullable().alter()
     t.string('password').notNullable()
   });
 };
