a47734289dd0ae25cb8f622235471012348eeb6f
Andrei <andrei@colourhood.org>
2018-05-02 19:44:47 -0700

    migrated seeding data into seed file

--- seeds/organizations_seeds.js
+++ seeds/organizations_seeds.js
@@ -1,14 +1,25 @@
 
-const organizationData = require('./organization_data');
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('organization').del()
     .then(() => {
       // Inserts seed entries
-      return knex('organization').insert(organizationData)
-        .then(() => {
-          return Promise.all()
-        });
+      return knex('organization').insert([
+        {
+            'name': 'Colourhood',
+            'hours': 'Mon, Tue, Wed, Thur, Fri,',
+            'services': '1, 2, 3, 4'
+        },
+        {
+            'name': 'Union Gospel Mission',
+            'hours': 'Mon, Tue, Wed, Thur, Fri, Sat, Sun',
+            'services': '1, 2, 3, 4, 5, 6, 7, 8'
+        },
+        {
+            'name': 'University Presbyterian Church',
+            'hours': 'Mon, Tue, Wed, Thur, Fri, Sat, Sun',
+            'services': '1, 3, 4'
+        }
+      ]);
     });
 };
